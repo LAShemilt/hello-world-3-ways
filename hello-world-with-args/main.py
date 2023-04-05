3d6f4a49b30de4635bc4fcbfabe5053cf57e1097
vpf26432 <laura.shemilt@rfi.ac.uk>
2023-04-05 10:39:16 +0100

    hello world with arguments and I/O

---
+++ hello-world-with-args/main.py
@@ -0,0 +1,28 @@
+import click
+
+def print_hi(name):
+    greeting = f'Hi, {name}'
+    print(greeting)
+    return greeting
+
+def save_greeting(fname, greeting):
+    with open (fname, 'w') as f:
+        f.writelines(greeting)
+@click.command()
+@click.option('--name', default="World", help='The person you want to greet.')
+@click.option('--fname', default=None, help='The path you want to save the greeting to')
+def greet(name, fname=None):
+
+    try:
+        greeting = print_hi(name)
+    except:
+        greeting = None
+        print("No name provided could not greet")
+
+    if fname and greeting:
+        save_greeting(fname, greeting)
+
+if __name__ == '__main__':
+    greet()
+
+
