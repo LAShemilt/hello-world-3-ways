5e171a3c6d3cd4b743ece693155ece1e23add2e4
vpf26432 <laura.shemilt@rfi.ac.uk>
2023-04-11 08:49:19 +0000

    additions to GUI to add name

--- hello-world-with-gui/src/application.py
+++ hello-world-with-gui/src/application.py
@@ -1,12 +1,12 @@
 import gi
 gi.require_version('Gtk', '4.0')
-gi.require_version("Adw", "1")
-from gi.repository import Gtk, Adw
+gi.require_version("Gdk", "4.0")
+from gi.repository import Gtk, Gdk
 from .application_window import AppWindow
 
 
 
-class HelloApp(Adw.Application):
+class HelloApp(Gtk.Application):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.connect('activate', self.on_activate)
--- hello-world-with-gui/src/application_window.py
+++ hello-world-with-gui/src/application_window.py
@@ -1,18 +1,51 @@
 import gi
 gi.require_version('Gtk', '4.0')
-gi.require_version("Adw","1")
-from gi.repository import Gtk, Adw
+gi.require_version("Gdk","4.0")
+from gi.repository import Gtk, Gdk
 class AppWindow(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs,
+        default_height=230,
+            default_width=500,)
 
-        self.box = Gtk.Box()
+        self.box= Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        
+        self.box2=Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        self.box3=Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        self.box4=Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         self.set_child(self.box)
+        self.box.append(self.box2)
+        self.box.append(self.box3)
+        self.box.append(self.box4)
+    
+        
 
         self.button = Gtk.Button(label="Hello")
-        self.box.append(self.button)
+        self.box2.append(self.button)
 
         self.button.connect('clicked', self.hello)
 
+        self.name_entry = Gtk.Entry()
+        self.box3.append(self.name_entry)
+
+
     def hello(self, button):
-        print("Hello world")
+        self._name = self.name_entry.get_text()
+        if self._name:
+           self.name_box=Gtk.Label(label=f"Hello {self._name}")
+        else:
+            self.name_box= Gtk.Label(label="Hello world")
+        self.box4.append(self.name_box)
+
+
+
+
+
+
+
+
+
+
+
+
+
